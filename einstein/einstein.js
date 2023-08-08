96e24ef8a1273e972523e75775aad1338b976af9
diegonciopilato <140232830+diegonciopilato@users.noreply.github.com>
2023-08-07 19:14:20 -0500

    Add files via upload

---
+++ einstein/einstein.js
@@ -0,0 +1,50 @@
+// Ejemplo de cambio de color del menú activo
+const menuLinks = document.querySelectorAll("nav a");
+
+menuLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        // Eliminar la clase 'active' de todos los enlaces
+        menuLinks.forEach(link => link.classList.remove("active"));
+
+        // Agregar la clase 'active' solo al enlace clicado
+        link.classList.add("active");
+    });
+});
+
+// Ejemplo de cambio de color del título del libro al pasar el mouse
+const bookTitle = document.querySelectorAll(".book-post h2 a");
+
+bookTitle.forEach(title => {
+    title.addEventListener("mouseover", () => {
+        title.style.color = "#FFA500";
+    });
+
+    title.addEventListener("mouseout", () => {
+        title.style.color = "red";
+    });
+});
+const audios = document.querySelectorAll('audio');
+
+audios.forEach((audio, index) => {
+    audio.addEventListener('ended', () => {
+        const nextAudio = audios[index + 1];
+        if (nextAudio) {
+            nextAudio.play();
+        }
+    });
+});
+
+const audioPlayers = document.querySelectorAll('.audio-section audio');
+
+audioPlayers.forEach((audio) => {
+    audio.addEventListener('play', handlePlay);
+});
+
+function handlePlay(event) {
+    const currentAudio = event.target;
+    audioPlayers.forEach((audio) => {
+        if (audio !== currentAudio && !audio.paused) {
+            audio.pause();
+        }
+    });
+}
